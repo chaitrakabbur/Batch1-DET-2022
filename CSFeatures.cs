c826fa2a88947438cb10f91fe3246cd2e8307046
chaitra <chaitra.kabbur@sonata-software.com>
2022-08-30 17:10:24 +0530

    hi

---
+++ CSFeatures.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class CSFeatures
+    {
+        public static void Main()
+        {
+            Action<string> action = (string name) => { Console.WriteLine($"Hi {name}"); };
+            //action.Invoke("Chaitra");
+            Action<string, string> action1 = (string s1, string s2) => { Console.WriteLine($"Hi {s1}, " + $"is having{s2}"); };
+
+            action.Invoke("Chaitra");
+            action1.Invoke("Chaitra", " a good day");
+
+            Action<int, string, string> printEmp = (int id, string name, string email) =>
+            {
+                Console.WriteLine($"id={id}, name={name}, email={email}");
+            };
+            printEmp(7575, "Chaitra", "CK@s.com");
+        }
+    }
+}
---
+++ IAuthor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal interface IEmail
+    {
+        string SendEmail();
+
+    }
+    public interface INotify
+    {
+        string SendSMS();
+    }
+
+    class Author : IEmail, INotify
+    {
+        public string Name { get ; set ; }
+        public string Email { get ; set ; }
+        public char Gender { get ; set ; }
+
+        public string SendEmail()
+        {
+            return $"Mail sent to {Name}, {Email},{Gender}";
+        }
+        public string SendSMS()
+        {
+            return $"{Name} subscribed SMS notifications";
+        }
+
+    }
+    class SITLAuthor :Author, IEmail
+    {
+
+        public string SendEmail()
+        {
+            return $"Mail sent to Name={Name}, Email={Email},Gender={Gender}";
+        }
+
+    }
+    class TestAuthor
+    {
+        public static void Main()
+        {
+            Author a = new Author { Name = "Chaitra", Email = "ck@s.com", Gender = 'F' };
+            SITLAuthor sa = new SITLAuthor { Name = "ABC", Email = "abc@s.com", Gender = 'F' };
+            Console.WriteLine(a.SendEmail());
+            Console.WriteLine(a.SendSMS());
+            Console.WriteLine(sa.SendEmail());
+        }
+    }
+}
